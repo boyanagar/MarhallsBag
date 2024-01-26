364ca2222d2d5bc7cb70624edf82daa01be00b41
boyanagar <121471365+boyanagar@users.noreply.github.com>
2024-01-26 15:02:22 -0600

    Update AddBagTest.java

--- mymarshalls1/src/test/java/test/com/AddBagTest.java
+++ mymarshalls1/src/test/java/test/com/AddBagTest.java
@@ -43,50 +43,68 @@ public class AddBagTest {
 
     @BeforeMethod
     public void setUpMethod() throws Exception {
-    System.setProperty("webdriver.chrome.driver", "c:\\data\\chromedriver.exe");
-    driver = new ChromeDriver();
-    baseUrl = "https://www.google.com/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    driver.manage().window().maximize();
+        System.setProperty("webdriver.chrome.driver", "c:\\data\\chromedriver.exe");
+        driver = new ChromeDriver();
+        baseUrl = "https://www.google.com/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        
     }
 
     @AfterMethod
     public void tearDownMethod() throws Exception {
     }
-    
-    @Test
-  public void testAddBag() throws Exception {
-    driver.get("https://www.marshalls.com/us/store/shop/womens-handbags-purses/_/N-3758044351?ln=4:1");
-    driver.findElement(By.xpath("//img[@alt=' ']")).click();
-    driver.get("https://www.marshalls.com/us/store/jump/product/women-women-handbags/Love-Is-Juicy-Backpack/4000221752?colorId=NS4154762&pos=1:1&N=3758044351");
-    driver.findElement(By.id("addItemToOrder")).click();
-    driver.findElement(By.linkText("View Bag")).click();
-    driver.get("https://www.marshalls.com/us/store/checkout/cart.jsp");
-  }
 
-     //@Test
-  public void testRemove() throws Exception {
-    driver.get("https://www.marshalls.com/us/store/checkout/cart.jsp?_requestid=4791871");
-    driver.findElement(By.xpath("//img[@alt='Cozy Plush Skinny Jeans']")).click();
-    driver.get("https://www.marshalls.com/us/store/jump/product/Cozy-Plush-Skinny-Jeans/4000237406?cs=cart&rrec=true");
-    driver.findElement(By.linkText("6")).click();
-    driver.findElement(By.id("addItemToOrder")).click();
-    driver.findElement(By.linkText("Keep Shopping")).click();
-    driver.findElement(By.id("usmm-tl-cat3620040p")).click();
-    driver.get("https://www.marshalls.com/us/store/shop/women/_/N-2906019403?tn=2");
-    driver.findElement(By.xpath("//div[@id='endeca-main']/div[2]/div/div[2]/ul/li[4]/a")).click();
-    driver.get("https://www.marshalls.com/us/store/shop/womens-handbags-purses/_/N-3758044351?ln=4:1");
-    driver.findElement(By.xpath("//img[@alt=' ']")).click();
-    driver.get("https://www.marshalls.com/us/store/jump/product/women-women-handbags/Love-Is-Juicy-Backpack/4000221752?colorId=NS4154762&pos=1:1&N=3758044351");
+    @Test(priority = 1)
+    public void testAddBag() throws Exception {
+        driver.manage().window().maximize();
+        driver.get("https://www.marshalls.com/us/store/shop/women/");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,450)", "");
+        driver.findElement(By.xpath("//img[@alt=' ']")).click();
+        driver.get("https://www.marshalls.com/us/store/jump/product/women-women-handbags/Love-Is-Juicy-Backpack/4000221752?colorId=NS4154762&pos=1:1&N=3758044351");
+        driver.findElement(By.id("addItemToOrder")).click();
+        driver.findElement(By.linkText("View Bag")).click();
+        driver.get("https://www.marshalls.com/us/store/checkout/cart.jsp");
+        driver.quit();
+    }
+
+    @Test(priority = 2)
+    public void testAddBag2() throws Exception {
+        driver.manage().window().maximize();
+        driver.get("https://www.marshalls.com/us/store/shop/new-arrivals/home/_/N-842114098+2241537684?ln=6:1");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,450)", "");
+        driver.findElement(By.xpath("//img[@alt='4pc Emma Bath Towel Set']")).click();
+        driver.get("https://www.marshalls.com/us/store/jump/product/home/new-arrivals/4pc-Emma-Bath-Towel-Set/4000227295?colorId=NS4155173&pos=1:1&N=842114098+2241537684");
+        driver.findElement(By.id("addItemToOrder")).click();
+        driver.findElement(By.linkText("Keep Shopping")).click();
+        driver.get("https://www.marshalls.com/us/store/shop/new-arrivals/home/_/N-842114098+2241537684?ln=6:1");
+        driver.findElement(By.xpath("//img[@alt='Hand Soap And Hand Lotion On Cutting Board']")).click();
+        driver.get("https://www.marshalls.com/us/store/jump/product/home/new-arrivals/Hand-Soap-And-Hand-Lotion-On-Cutting-Board/4000233397?colorId=NS4155481&pos=1:2&N=842114098+2241537684");
+        driver.findElement(By.id("quantity-4000233397")).click();
+        new Select(driver.findElement(By.id("quantity-4000233397"))).selectByVisibleText("3");
+        driver.findElement(By.id("addItemToOrder")).click();
+        driver.findElement(By.linkText("View Bag")).click();
+        driver.get("https://www.marshalls.com/us/store/checkout/cart.jsp");
+        driver.quit();
+    }
+
+    //@Test(priority = 3)
+    public void testRemove() throws Exception {
+    driver.manage().window().maximize();
+    driver.get("https://www.marshalls.com/us/store/shop/beauty-accessories/_/N-2063148638?tn=5");
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+    js.executeScript("window.scrollBy(0,450)", "");
+    driver.findElement(By.xpath("//img[@alt='Detangler For Thick Hair']")).click();
+    driver.get("https://www.marshalls.com/us/store/jump/product/beauty-accessories/Detangler-For-Thick-Hair/4000250402?colorId=NS4154949&pos=1:5&N=2063148638");
     driver.findElement(By.id("addItemToOrder")).click();
     driver.findElement(By.linkText("View Bag")).click();
     driver.get("https://www.marshalls.com/us/store/checkout/cart.jsp");
-    driver.findElement(By.linkText("Edit")).click();
+    driver.findElement(By.xpath("//form[@id='cartUpdateForm']/ul/li[3]/div[2]/span[3]/a")).click();
     driver.findElement(By.id("quantity")).click();
     new Select(driver.findElement(By.id("quantity"))).selectByVisibleText("0");
     driver.findElement(By.name("removeAndAddItemToOrder")).click();
-    driver.findElement(By.xpath("//form[@id='cartUpdateForm']/ul/li/div[3]/a")).click();
+     driver.quit();
   }
 
-  
 }
